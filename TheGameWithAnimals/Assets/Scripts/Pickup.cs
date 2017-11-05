6433bfd3ddf4843d5a42326d362a06f1db66d8b9
indijones4 <indijones4@gmail.com>
2017-11-05 17:22:09 +1100

    adding basic game objects and one script

---
+++ TheGameWithAnimals/Assets/Scripts/Pickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		//code that makes the pickup slowly rotate on the spot
+		transform.Rotate (new Vector3 (0, 45, 0) * Time.deltaTime);
+	}
+}
