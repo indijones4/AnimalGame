7d5224285af38f49998069a96bef9a7246cd0dba
indijones4 <indijones4@gmail.com>
2017-11-23 19:23:15 +1100

    Did things broke things going to try things

---
+++ TheGameWithAnimals/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform player; 
+
+
+
+
+	// Use this for initialization
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- TheGameWithAnimals/Assets/Scripts/GameController.cs
+++ TheGameWithAnimals/Assets/Scripts/GameController.cs
@@ -9,8 +9,8 @@ public class GameController : MonoBehaviour {
 	//variables
 	public static GameController instance = null;
 	public bool gameOver = false;
-	Text dieText;
-	Text winText;
+	public Text dieText;
+	public Text winText;
 
 	//awake is called before start
 	void Awake () 
@@ -48,8 +48,11 @@ public class GameController : MonoBehaviour {
 	//a corountinue to be called in the playerscript for when the player dies
 	public void PlayerDied()
 	{
+		//GUIElement temp;
+		//temp = GameObject.FindGameObjectWithTag("GameOverText");
+		//dieText = temp.<Text>
 		gameOver = true;
-		dieText.text = "You died... \n press spacebar to try again";
+		//dieText.text = "You died... \n press spacebar to try again";
 		Time.timeScale = 0;
 	}
 
@@ -57,7 +60,7 @@ public class GameController : MonoBehaviour {
 	public void PlayerWon()
 	{
 		gameOver = true;
-		winText.text = "YOU WON! \n press spacebar to try again";
+		//winText.text = "YOU WON! \n press spacebar to try again";
 		Time.timeScale = 0;
 	}
 
